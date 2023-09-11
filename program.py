10a7b0f27db73c49b987c14c6d0287a3aa742b01
Панюкова <dima.laskov.ru@mail.ru>
2023-09-11 20:02:20 +0300

    Receiving the code

--- program.py
+++ program.py
@@ -6,6 +6,11 @@ name = input()
 time.sleep(2)
 print("Хех, здравствуй " + name + "!")
 time.sleep(2)
+print("А СЕЙЧАС ПОЖАЛУЙСТА НЕ ПУГАЙСЯ")
+time.sleep(2)
+print("И ДАЙ МНЕ КОД ОТ АВТОМТА ПО ОСНОВАМ ПРОГРАММНОЙ ИНЖЕНЕРИИ")
+code = input()
+time.sleep(2)
 
 
 
